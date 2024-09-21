0f87fe2dcbc0ea1c6c04f4fbe5d4614d1d8d83d7
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-09-21 15:23:25 +0100

    c

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,14 +1,14 @@
-n1 = float(input("Enter the first number:"))
-n2 = float(input("Enter the second number:"))
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
 match input("Choose the operation (+, -, *, /):"):
     case "+" :
-        print("The result is {}.".format(n1 + n2))
+        print("The result is {}.".format(num1 + num2))
     case "-" :
-        print("The result is {}.".format(n1 - n2))
+        print("The result is {}.".format(num1 - num2))
     case "*" :
-        print(f"The result is {n1 * n2}.")
+        print(f"The result is {num1 * num2}.")
     case "/" :
-        if n2 == 0:
+        if num2 == 0:
             print("Cannot divide by zero.")
         else :
-            print("The result is {}.".format(n1 / n2))
+            print("The result is {}.".format(num1 / num2))
