46605809e494e98719d4ccaf23769cbf42dd9ad7
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-09-26 09:33:34 +0100

    .\fns_and_dsa\

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -11,9 +11,8 @@ def main():
     while True:
         display_menu()
         choice = input("Enter your choice: ")
-
         if choice == '1':
-            a = input(("Enter the item to add:"))
+            a = int(input(("Enter the item to add:")))
             shopping_list.append(a)
         elif choice == '2':
             a = input(("what's the item you need to remove it :"))
