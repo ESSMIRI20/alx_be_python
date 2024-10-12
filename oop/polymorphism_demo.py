99be72301592af6f0b0946bebc46e0cd3cb31bc1
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-10-12 11:57:13 +0100

    .\oop\

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,16 @@
+import math
+class Shape:
+    def __init__(self, length = 0, width = 0, radius = 0):
+        self.lenght = length
+        self.width = width
+        self.radius = radius
+
+class Rectangle(Shape):
+    def area(self):
+        return self.lenght * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__(radius = radius)
+    def area(self):
+        return math.pi * self.radius ** 2
