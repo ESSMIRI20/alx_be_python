a1ca2115843426667efcf96268fa14b2a477c0cd
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-10-12 12:03:48 +0100

    .\oop\

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,15 +1,13 @@
 import math
 class Shape:
     def __init__(self, length = 0, width = 0, radius = 0):
-        self.lenght = length
+        self.length = length
         self.width = width
         self.radius = radius
 
 class Rectangle(Shape):
-    def __init__(self, length, width):
-        super().__init__(length = length, width = width)
     def area(self):
-        return self.lenght * self.width
+        return self.length * self.width
 
 class Circle(Shape):
     def __init__(self, radius):
